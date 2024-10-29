8ca348171bebb8fc61b4fe002f8616548624e5bf
Muhammad Abdullah Bin Asim <90253127+SMSabdullah002@users.noreply.github.com>
2024-10-29 09:25:43 +0500

    1.1 Store the dataset in Memory: (Status: Done)  Task3 ->  Implemented Member Function  to load data to BST for Both Player and Game

--- DS_B_A3_i232621.cpp
+++ DS_B_A3_i232621.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
+#include<fstream>
 using namespace std;
-//Simple Class for game status
+//Simple Node Class for game status Link List
 class Games_played_class{
     public:
     string gameID;
     float hours_played;
     int achievements_unlock;
-    Games_played_class(string id,float h,int ach):gameID(id),hours_played(h),achievements_unlock(ach){}
+    Games_played_class* next;
+    Games_played_class(string &id,float h,int ach):gameID(id),hours_played(h),achievements_unlock(ach),next(NULL){}
     
 };
 //Players Node : Will be used in players Database
@@ -17,10 +19,15 @@ class Player{
     string phone_number;
     string email;
     string password;
-    Games_played_class Logs;
+    Games_played_class* GP_head;
     Player* left;
     Player* right;
-    Player(string n,string id,string ph,string em,string pass,Games_played_class log):name(n),playerID(id),phone_number(ph),email(em),password(pass),Logs(log),left(NULL),right(NULL){}
+    Player(string &n,string &id,string &ph,string &em,string &pass,Games_played_class* log):name(n),playerID(id),phone_number(ph),email(em),password(pass),GP_head(log),left(NULL),right(NULL){}
+    void addGameLog(Games_played_class* log)
+    {
+        log->next=  GP_head;
+        GP_head=log;
+    }
 };
 //Games Node : Will be used in Games Database
 class Game{
@@ -33,7 +40,7 @@ class Game{
     int downloads;
     Game* left;
     Game* right;
-    Game(string id,string n,string dev,string pub,float file,int down):gameID(id),Name(n),Developer(dev),Publisher(pub),File_size_in_GB(file),downloads(down),left(NULL),right(NULL){}
+    Game(string &id,string &n,string &dev,string &pub,float file,int down):gameID(id),Name(n),Developer(dev),Publisher(pub),File_size_in_GB(file),downloads(down),left(NULL),right(NULL){}
 };
 
 
@@ -43,11 +50,60 @@ class Players_DataBase{
     Player* root;  
     Players_DataBase():root(NULL){}
 
-    void insert(Player* player){
+    void insertPlayer(Player* player){
 
     }
     void Loading_Data_Player(string& fileName,int seed, int limit)
     {
+        srand(seed);
+       ifstream file(fileName);
+       string line;
+
+       while(getline(file,line))
+       {
+         if(rand()% 1000 < limit) continue; 
+
+         int pos = 0;
+         string Player_id,name,phone,email,password,game_id;
+         float hours=0;
+         int achievement=0;
+
+         pos = line.find(',');
+         Player_id=line.substr(0,pos);
+         line.erase(0,pos + 1);
+
+         pos = line.find(',');
+         name=line.substr(0,pos);
+         line.erase(0,pos + 1);
+
+        pos = line.find(',');
+         phone=line.substr(0,pos);
+         line.erase(0,pos + 1);
+
+         pos = line.find(',');
+         email=line.substr(0,pos);
+         line.erase(0,pos + 1);
+
+         pos = line.find(',');
+         password=line.substr(0,pos);
+         line.erase(0,pos + 1);
+
+         pos = line.find(',');
+        game_id=line.substr(0,pos);
+         line.erase(0,pos + 1);
+
+         pos = line.find(',');
+         hours= stof(line.substr(0, pos));
+         line.erase(0,pos + 1);
+
+        achievement = stoi(line);
+        
+        Games_played_class* log = new Games_played_class(game_id,hours,achievement);
+        Player* player = new Player(Player_id,name,phone,email,password,log);
+
+        insertPlayer(player);
+       }
+       file.close();
 
     }
 
@@ -57,12 +113,54 @@ class Games_DataBase{
     Game* root;
     Games_DataBase():root(NULL){}
      
-    void insert(Game* player){
+    void insertGame(Game* player){
+
 
     }
     void Loading_Data_Game(string& fileName,int seed, int limit)
     {
+       srand(seed);
+       ifstream file(fileName);
+       string line;
 
+       while(getline(file,line))
+       {
+         if(rand()% 1000 < limit) continue; 
+
+         int pos = 0;
+         string game_id,name,dev,publisher;
+         float filesize=0;
+         int  downloads=0;
+
+         pos = line.find(',');
+        game_id=line.substr(0,pos);
+         line.erase(0,pos + 1);
+
+         pos = line.find(',');
+         name=line.substr(0,pos);
+         line.erase(0,pos + 1);
+
+        pos = line.find(',');
+         dev=line.substr(0,pos);
+         line.erase(0,pos + 1);
+
+         pos = line.find(',');
+         publisher=line.substr(0,pos);
+         line.erase(0,pos + 1);
+
+
+         pos = line.find(',');
+         filesize= stof(line.substr(0, pos));
+         line.erase(0,pos + 1);
+
+        downloads = stoi(line);
+        
+       
+        Game* game = new Game(game_id,name,dev,publisher,filesize,downloads);
+
+        insertGame(game);
+       }
+       file.close();
     }
 };
 int main()
